e5b6994abd853a724fa7605f8a5bd6fabea067cf
Ghanshyambunkar <ghanshyambunkar016@gmail.com>
2025-05-02 10:57:59 +0530

    update

--- init/index.js
+++ init/index.js
@@ -9,13 +9,13 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://bunkarghanshyam3:J3R9D7YXEGTkQySr@cluster0.cmyc7.mongodb.net/');
+  await mongoose.connect('mongodb+srv://ghanshyambunkar016:VQmVV6TftYNLhvPC@cluster0.mk7bd.mongodb.net/');
 }
 
 const initDB=async()=>{
-    await Listing.deleteMany({});
-    // initdata.data=initdata.data.map((obj)=>({...obj,owner:"67a5c53e57edd2c9f78baa83"}));
-    // await Listing.insertMany(initdata.data);
+    // await Listing.deleteMany({});
+    initdata.data=initdata.data.map((obj)=>({...obj,owner:"67a5eb18570d9c9fc0cadc60"}));
+    await Listing.insertMany(initdata.data);
     console.log("data was initialized..");
 };
 
